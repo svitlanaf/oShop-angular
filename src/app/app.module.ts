6621fd74c17e8a525fb8af2c165b35586a6262c2
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-22 15:03:37 -0700

    Add products displaying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,7 +65,7 @@ export const firebaseConfig = {
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: ProductsComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component: LoginComponent },

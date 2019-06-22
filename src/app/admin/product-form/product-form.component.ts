5c387dae066695d64234eedbba471ac35d4815e5
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-22 00:06:07 -0700

    Add delete for products

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -29,8 +29,15 @@ export class ProductFormComponent implements OnInit {
   save(product) {
     if (this.id) this.productService.update(this.id, product);
     else this.productService.create(product);
-    
+
     this.router.navigate(['/admin/products']);
+  }
+
+  delete() {
+    if (!confirm('Are you sure you want to delete this product?')) return;
+    this.productService.delete(this.id);
+    this.router.navigate(['/admin/products']);
+    
   }
 
   ngOnInit() {

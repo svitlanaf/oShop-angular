9fed615bfd26e7c4581c60efb56e785d9cf9a495
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-22 14:30:14 -0700

    Add fixes for user redirection and product form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,15 @@ import { Router } from '@angular/router';
 export class AppComponent {
   constructor(private userService: UserService, private auth: AuthService, router: Router) {
     auth.user$.subscribe(user => {
-      if (user) {
-        userService.save(user);
+      if (!user) return;
+
+      userService.save(user);
 
         let returnUrl = localStorage.getItem('returnUrl');
+        if (!returnUrl) return;
+
+        localStorage.removeItem('returnUrl');
         router.navigateByUrl(returnUrl);
-      }
     });
   }
 }

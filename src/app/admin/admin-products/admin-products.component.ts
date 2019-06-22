2a22b87eae157d6b8d9b76fc627dbfe88a08041f
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-22 00:26:41 -0700

    Add filtering products by title

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,18 +1,29 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
-  constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
-   }
+export class AdminProductsComponent implements OnDestroy {
+  products: {title: string}[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+    this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
 
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }

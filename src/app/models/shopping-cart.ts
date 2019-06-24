75df49d94a34c173d362f8c8d91fb731a3c006a7
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-23 23:57:41 -0700

    Add displaying total price in shopping cart

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,11 +1,24 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {
+    items: ShoppingCartItem[] = [];
 
-    constructor(public items: ShoppingCartItem[]) {}
+    constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        for (let productId in itemsMap) {
+            let item = itemsMap[productId];
+        this.items.push(new ShoppingCartItem(item.product, item.quantity))
+        }
+    }
 
-    get productIds() {
-        return Object.keys(this.items);
+    // get productIds() {
+    //     return Object.keys(this.items);
+    // }
+
+    get totalPrice() {
+        let sum = 0;
+        for (let productId in this.items)
+            sum += this.items[productId].totalPrice;
+            return sum;
     }
 
     get totalItemsCount() {

20156ec323e1163325d3b3f950dceec4add80b5d
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-24 01:22:14 -0700

    Add clearing shopping cart

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -8,7 +8,9 @@ export class ShoppingCartItem {
     quantity: number;
 
 
-    // constructor(public product: Product, public quantity: number) {}
+    constructor(init?: Partial<ShoppingCartItem>) {
+        Object.assign(this, init);
+    }
 
     get totalPrice() { return this.price * this.quantity; }
 }

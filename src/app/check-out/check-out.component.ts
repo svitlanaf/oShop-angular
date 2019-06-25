9e417baec7d00dd76f8fe352b177cfef677caa5a
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-24 22:39:36 -0700

    Add userd id to checkout component

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,3 +1,5 @@
+import { UserService } from './../user.service';
+import { AuthService } from './../auth.service';
 import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
@@ -12,23 +14,29 @@ import { OrderService } from '../order.service';
 export class CheckOutComponent implements OnInit, OnDestroy { 
   shipping = {}; 
   cart: ShoppingCart;
-  subscription: Subscription;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
   constructor(
+    private authService: AuthService,
     private orderService: OrderService,
     private shoppingCartService: ShoppingCartService) {}
 
   async ngOnInit() {
     let cart$ = await this.shoppingCartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.authService.user$.subscribe(user => this.userId = user.uid);
   }
   
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
   placeOrder() {
     let order = {
+      userId: this.userId,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
       items: this.cart.items.map(i => {
